dcddc1d96260aea655164ce369ea2e2f2ac7e452
Rahul Dusaje <60280577+RahulDusajeFSD@users.noreply.github.com>
2024-09-29 08:56:48 -0400

    added vpc_security_group

--- main.tf
+++ main.tf
@@ -13,16 +13,68 @@ data "aws_ami" "app_ami" {
   }
 
   owners = ["979382823631"] # Bitnami
+
+
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
 
-  
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
   instance_type = var.instance_type
 
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog"{
+
+  name        = "blog"
+  description = "Inbound - HTTP/ HTTPS. Outbound = 0.0.0.0 Everything" 
+  
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in"{
+  
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = data.aws_security_group.blog.id
+
+}
+
+resource "aws_security_group_rule" "blog_https_in"{
+  
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = data.aws_security_group.blog.id
+
+}
+
+resource "aws_security_group_rule" "outbound"{
+  
+  type      = "egress"
+  from_port = 0 // Means any port
+  to_port   = 0
+  protocol  = -1 // Means all protocols applied
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = data.aws_security_group.blog.id
+
 }
