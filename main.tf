d80f403ffc0a6326ef440b552ccc191b186ca27e
Rahul Dusaje <60280577+RahulDusajeFSD@users.noreply.github.com>
2024-09-30 18:05:50 -0400

    create new dev vpc

--- main.tf
+++ main.tf
@@ -22,13 +22,33 @@ data "aws_vpc" "default" {
 }
 
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
 
   vpc_security_group_ids = [module.blog_sg.security_group_id] // Referencing to security group module (blog_sg) instead of resource(aws_security_group).
  
+ 
   instance_type = var.instance_type
 
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
@@ -42,7 +62,7 @@ module "blog_sg" {
 
   name = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
